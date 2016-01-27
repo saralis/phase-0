c56dbb6052515a555197089962fafeece2017441
Saralis Rivera <saralis.rivera@gmail.com>
2016-01-27 03:43:28 -0500

    Update Control Flow Triangle Assignment

--- week-4/triangle-side/my_solution.rb
+++ week-4/triangle-side/my_solution.rb
@@ -1,8 +1,18 @@
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 
 # Your Solution Below
 
 def valid_triangle?(a, b, c)
-  # Your code goes here!
+  if (a==0) || (b==0) || (a==0)
+    return false
+  elsif (a+b<c) || (b+c<a) || (a+c<b)
+    return false
+  elsif (a==b) && (b==c) && (a==c)
+    return true
+  elsif (a*2+b*2) && (a*2+c*2) && (b*2+c*2)
+    return true
+  else
+    return false
+  end
 end
