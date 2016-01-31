23c5902c7eef8cf30e4c51757826c9be6867f077
Saralis Rivera <saralis.rivera@gmail.com>
2016-01-31 12:44:43 -0500

    Create file for group research assignment

---
+++ week-5/group-research-methods/my_solution.rb
@@ -0,0 +1,35 @@
+# Research Methods
+
+# I spent [] hours on this challenge.
+
+i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
+my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
+
+# Person 5
+def my_array_splitting_method([[integers],[value]])
+
+end
+
+def my_hash_splitting_method(source, age)
+
+end
+
+# Identify and describe the Ruby method(s) you implemented.
+#
+#
+#
+
+
+# Release 1: Identify and describe the Ruby method you implemented. Teach your
+# accountability group how to use the methods.
+#
+#
+#
+
+
+# Release 3: Reflect!
+# What did you learn about researching and explaining your research to others?
+#
+#
+#
+#
