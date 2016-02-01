dbfb88207bc89eb4c57270004b930aa881303104
Saralis Rivera <saralis.rivera@gmail.com>
2016-01-31 21:11:03 -0500

    Update to group method solution for person 5

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -1,15 +1,26 @@
+
+
 # Research Methods
 
 # I spent [] hours on this challenge.
 
 i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
-my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
 
-# Person 5
-def my_array_splitting_method([[integers],[value]])
-
+# Person 5       # ([[integers],[all other value]])
+def my_array_splitting_method(i_want_pets)
+  i_want_pets.each.split(",")
+  if x == integer
+    array_1 << x
+  else
+    array_2 << x
 end
 
+
+end
+puts my_array_splitting_method([])
+
+
+#my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
 def my_hash_splitting_method(source, age)
 
 end
