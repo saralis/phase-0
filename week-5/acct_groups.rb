94f40ea1b31f9bdc20ef564193e9d1889d46bb25
Saralis Rivera <saralis.rivera@gmail.com>
2016-02-02 22:08:58 -0500

    Update to GPS week-5 and commas challenge

---
+++ week-5/acct_groups.rb
@@ -0,0 +1,18 @@
+# Pseudocode
+
+#In this challenge, you will make your own method to automatically create accountability groups from a list of names. You'll want to make of the People in your cohort. Try to get everyone into an accountability group of 4 or 5. Be sure everyone is in a group of at least 3 -- It's no fun if someone is in a group by themself or with one other person.
+# Input: List of names
+# Steps:
+# create a variable and set equal to the list of names
+# create a container to hold a list of strings
+# split the string by a separator(,) into arrays of groups of 4 or 5
+# Output: Accountability groups as arrays
+
+list_of_names = "Alivia Blount, Alyssa Page, Alyssa Ransbury, Andria Reta, Austin Dorff, Autumn McFeeley, Ayaz Uddin, Ben Glamarino, Benjamin Heidebrink, Bethelhem Assefa, Robert Reith, Dana Breen, Brett Ripley, Rene Castillo, Justin Chang, Che Sanders, Chris Hendrson, Colette Pitamba, Connor Reaumond, Cyrus Vattes, Dan Heintzelman, David Lange, Eduardo Bueno, Elizabeth Roche, FJ Collins Jr., Frances Pangilinan, Ian Fricker, Ian Throp, Ivy Vetor, Jack Baginski, Jack Hamilton, Jillian Campbell, John Craigie, John Holman, John Maguire, John Pults, Jones Melton, Kenton Lin, Kevin Serrano, Kevin Sullivan, Kyle Rombach, Laura Montoya, Luis Ybarra, Chorlotte Manetta, Martina Osteyee-Hoffman, Megan Swanby, Michael London, Michael Wang, Michael Yao, Mike Gwozdek, Miqueas Hernandez, Mitchell Kroska, Norberto Carceres, Patrick Skelley, Peter Kang, Philip Chung, Philip Chung, Phillip Barnett, Pietro Martini, Robbie Santos, Rokas Simkonis, Ronesh Ghoshal, Ryan Nebuda, Ryan Smith, Saralis Rivera, Sepand Assadi, Spencer Alexander, Stephanie Major, Taylor Daugherty, Thomas Farr, Maeve Tierney, Tori Huang, Alexander Williams, Victor Wong, Xin Zhang, Zach Barton"
+
+def accountability_group(list_of_names, group)
+  account_array = list_of_names.split(",")
+
+
+  p accountability_group
+end
--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -1,6 +1,6 @@
 # Numbers to Commas Solo Challenge
 
-# I spent [] hours on this challenge.
+# I spent [5] hours on this challenge.
 
 
 
@@ -11,12 +11,10 @@
 # What is the output?
 # Comma-separated integer as a string
 # What are the steps needed to solve the problem?
-=begin
-WHILE integer index is greater than or equal to -3 no comma is needed
-ELSE comma is added to integer with index divisible by -3
-=end
+#WHILE integer index is greater than or equal to -3 no comma is needed
+#ELSE comma is added to integer with index divisible by -3
 
-=begin
+
 # 1. Initial Solution
 def separate_comma (integer)
   if (integer >= 1000) && (integer <= 999999)
@@ -26,25 +24,27 @@ elsif (integer >= 1000000) && (integer <= 99999999)
   else return integer.to_s
   end
 end
-=end
+
 
 # 2. Refactored Solution
-def separate_comma (integer)
+#def separate_comma (integer)
 
-end
+#end
 
 
 
 # 3. Reflection
-=begin
-What was your process for breaking the problem down? What different approaches did you consider?
-Was your pseudocode effective in helping you build a successful initial solution?
 
-Yes, for my initial code I stuck with the IF/ELSE statement and looked for methods to insure
+#What was your process for breaking the problem down? What different approaches did you consider?
 
-What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them (any difficulties, etc.). Did it/they significantly change the way your code works? If so, how?
-How did you initially iterate through the data structure?
-Do you feel your refactored solution is more readable than your initial solution? Why?
+#Was your pseudocode effective in helping you build a successful initial solution?
+
+#Yes, for my initial code I stuck with the IF/ELSE statement and looked for methods to ensure
+
+#What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them (any difficulties, etc.). Did it/they significantly change the way your code works? If so, how?
+
+#How did you initially iterate through the data structure?
+
+#Do you feel your refactored solution is more readable than your initial solution? Why?
 
 
-=end
