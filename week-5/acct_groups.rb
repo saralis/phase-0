6a132305cc42e626ec189b9266ed6bd7a4641d40
Saralis Rivera <saralis.rivera@gmail.com>
2016-02-04 00:04:34 -0500

    Complete account group challenge and update to Week-5 GPS

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -8,11 +8,35 @@
 # split the string by a separator(,) into arrays of groups of 4 or 5
 # Output: Accountability groups as arrays
 
-list_of_names = "Alivia Blount, Alyssa Page, Alyssa Ransbury, Andria Reta, Austin Dorff, Autumn McFeeley, Ayaz Uddin, Ben Glamarino, Benjamin Heidebrink, Bethelhem Assefa, Robert Reith, Dana Breen, Brett Ripley, Rene Castillo, Justin Chang, Che Sanders, Chris Hendrson, Colette Pitamba, Connor Reaumond, Cyrus Vattes, Dan Heintzelman, David Lange, Eduardo Bueno, Elizabeth Roche, FJ Collins Jr., Frances Pangilinan, Ian Fricker, Ian Throp, Ivy Vetor, Jack Baginski, Jack Hamilton, Jillian Campbell, John Craigie, John Holman, John Maguire, John Pults, Jones Melton, Kenton Lin, Kevin Serrano, Kevin Sullivan, Kyle Rombach, Laura Montoya, Luis Ybarra, Chorlotte Manetta, Martina Osteyee-Hoffman, Megan Swanby, Michael London, Michael Wang, Michael Yao, Mike Gwozdek, Miqueas Hernandez, Mitchell Kroska, Norberto Carceres, Patrick Skelley, Peter Kang, Philip Chung, Philip Chung, Phillip Barnett, Pietro Martini, Robbie Santos, Rokas Simkonis, Ronesh Ghoshal, Ryan Nebuda, Ryan Smith, Saralis Rivera, Sepand Assadi, Spencer Alexander, Stephanie Major, Taylor Daugherty, Thomas Farr, Maeve Tierney, Tori Huang, Alexander Williams, Victor Wong, Xin Zhang, Zach Barton"
+names = ["Alivia Blount","Alyssa Page","Alyssa Ransbury","Andria Reta","Austin Dorff","Autumn McFeeley","Ayaz Uddin","Ben Giamarino","Benjamin Heidebrink","Bethelhem Assefa","Robert Reith","Dana Breen","Brett Ripley","Rene Castillo","Justin J Chang","Ché Sanders","Chris Henderson","Colette Pitamba","Connor Reaumond","Cyrus Vattes","Dan Heintzelman","David Lange","Eduardo Bueno","Elizabeth Roche","FJ Collins Jr.","Frances Pangilinan","Ian Fricker","Ian Thorp","Ivy Vetor","Jack Baginski","Jack Hamilton","Jillian Campbell","John Craigie","John Holman","John Maguire","John Pults","Jones Melton","Kenton Lin","Kevin Serrano","Kevin Sullivan","Kyle Rombach","Laura Montoya","Luis Ybarra","Charlotte Manetta","Martina Osteyee-Hoffman","Megan Swanby","Michael London","Michael Wang","Michael Yao","Mike Gwozdek","Miqueas Hernandez","Mitchell Kroska","Norberto Caceres","Patrick Skelley","Peter Kang","Philip Chung","Phillip Barnett","Pietro Martini","Robbie Santos","Rokas Simkonis","Ronesh Ghoshal","Ryan Nebuda","Ryan Smith","Saralis Rivera","Sepand Assadi","Spencer Alexander","Stephanie Major","Taylor Daugherty","Thomas Farr","Maeve Tierney","Tori Huang","Alexander Williams","Victor Wong","Xin Zhang","Zach Barton"]
 
-def accountability_group(list_of_names, group)
-  account_array = list_of_names.split(",")
+def accountability_group(list_of_names)
+  account_group = []
+  list_of_names.shuffle.each_slice(4) {|x| account_group << x}
 
 
-  p accountability_group
+p account_group
 end
+accountability_group(names)
+
+
+
+# What was the most interesting and most difficult part of this challenge?
+
+# The most difficult part for me was passing the data appropriately. After reviewing my unsuccessful code, I found that I had uncessessary lines of code and was calling the wrong elements.
+
+# Do you feel you are improving in your ability to write pseudocode and break the problem down?
+
+# Somewhat. I think I left frustration and anxiety get in the way this week so I didn't take time to really think through the pseudocode to have a more successful intial solution.
+
+# Was your approach for automating this task a good solution? What could have made it even better?
+
+# I think it was a good solution. I could have tried some more of the complexity tasks, but didn't due to time constraints.
+
+# What data structure did you decide to store the accountability groups in and why?
+
+# I went with arrays this time arround because it seemed like the most logical and simplest approach.
+
+# What did you learn in the process of refactoring your initial solution? Did you learn any new Ruby methods?
+
+# I used methods that I had researched previously for this approach. I learned that there are many approaches that could be taken within the same data structures. Utilizing the different methods can change how the data is returned when using arrays.
