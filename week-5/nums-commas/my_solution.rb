aac1742c5bbf337359f1d76cb90acb957a9f60ca
Saralis Rivera <saralis.rivera@gmail.com>
2016-02-03 20:15:32 -0500

    Update to commas challenge

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -16,20 +16,20 @@
 
 
 # 1. Initial Solution
-def separate_comma (integer)
-  if (integer >= 1000) && (integer <= 999999)
-  return integer.to_s.insert(-4,',')
-elsif (integer >= 1000000) && (integer <= 99999999)
-  return integer.to_s.insert(-4,',').insert(-8,',')
-  else return integer.to_s
-  end
-end
+#def separate_comma(integer)
+#  if (integer >= 1000) && (integer <= 999999)
+#  return integer.to_s.insert(-4,',')
+#elsif (integer >= 1000000) && (integer <= 99999999)
+#  return integer.to_s.insert(-4,',').insert(-8,',')
+ # else return integer.to_s
+ # end
+#end
 
 
 # 2. Refactored Solution
-#def separate_comma (integer)
-
-#end
+def separate_comma(integer)
+  integer.to_s.reverse.insert(-4,',').insert(-8,',').reverse
+end
 
 
 
@@ -37,14 +37,20 @@ end
 
 #What was your process for breaking the problem down? What different approaches did you consider?
 
+# I thought about how to change integers to strings given integers are presented in Ruby without any commas. I also thought about how best to tell the program where to place the commas. Utilizing the correct indices seemed like the right approach.
+
 #Was your pseudocode effective in helping you build a successful initial solution?
 
-#Yes, for my initial code I stuck with the IF/ELSE statement and looked for methods to ensure
+# Yes, my pseudocode was on the right track. I mad a few adustments when it came to getting my code to work and comma placement, but for the most part it was the right thought process.
 
 #What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them (any difficulties, etc.). Did it/they significantly change the way your code works? If so, how?
 
+# I had a difficult time finding methods that made sense to me and worked. I found some methods online while doing research that looked much cleaner, but at this point didn't make much sense to me.
+
 #How did you initially iterate through the data structure?
+# I utilized IF/ELSE for the initial solution to accounted for the integer ranges we were looking at for this challenge.
 
 #Do you feel your refactored solution is more readable than your initial solution? Why?
 
+# No, I actually think the initial solution is more readable because you can follow the specific steps/ranges being looked at for the comma placement.
 
