4e0c1f5288caafa488d5242317e7befd581f2919
Saralis Rivera <saralis.rivera@gmail.com>
2016-02-10 22:17:11 -0500

    Update to week-6 die 2 assignment

--- week-6/die-2/my_solution.rb
+++ week-6/die-2/my_solution.rb
@@ -1,14 +1,19 @@
 # Die Class 2: Arbitrary Symbols
 
 
-# I worked on this challenge [by myself].
-# I spent [#] hours on this challenge.
+# I worked on this challenge by myself.
+
+# I spent 5 hours on this challenge.
 
 # Pseudocode
 
 # Input: labels
 # Output: number of sides and rolls
 # Steps:
+# begin initializing method for the class
+# IF the argument does not meet the initial requirement, raise an argument error
+# create method for identifiying the elements of the array
+# create method for determining the rolls for the 'die'
 
 
 # Initial Solution
@@ -22,7 +27,7 @@ class Die
   end
 
   def sides
-    @labels
+    @labels = @labels.length
   end
 
   def roll
@@ -34,21 +39,20 @@ die = Die.new(['A','B','C','D','E','F'])
 puts die.sides
 puts die.roll
 
-
-# Refactored Solution
-
-
-
-
-
-
-
-
 # Reflection
+
 # What were the main differences between this die class and the last one you created in terms of implementation? Did you need to change much logic to get this to work?
+
+# There wasn't really any difference in the logic between the first die challenge and this one for the first two methods. Similarly to the first, I struggled with the last method and finding a working solution to address the test failures.
 
 # What does this exercise teach you about making code that is easily changeable or modifiable?
 
+# Making code changable allows for easier manipulation/adjustments at a later time.
+
 # What new methods did you learn when working on this challenge, if any?
 
+# I've used for strings before, but this time, I used the .length method on an array.
+
 # What concepts about classes were you able to solidify in this challenge?
+
+# I solidified my understanding of control flow.
