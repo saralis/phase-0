1d1b21cdebf42625b245f879ef09d6e0f6d6d93c
Saralis Rivera <saralis.rivera@gmail.com>
2016-02-12 08:54:09 -0500

    Add BINGO game

--- week-6/bingo_solution.rb
+++ week-6/bingo_solution.rb
@@ -7,8 +7,10 @@
 # Outline:
 
 # Create a method to generate a letter ( b, i, n, g, o) and a number (1-100)
+# define the method and identify variable for bingo letters and set to a hash. identify a range for a number variable.
 
 # Define a method to randomly select a letter(b, i, n, g, or o) and a number between 1 and 100.
+# set numbers to a random select method and to randomly select an index placement for the letter.
 
 # Check the called column for the number called.
   #fill in the outline here
@@ -28,29 +30,30 @@ class BingoBoard
 
   def initialize(board)
     @bingo_board = board
+    @bingo_index = {"B" => 0,"I" => 1,"N" => 2,"G" => 3,"0" => 4}
+    @bingo_index_random = @bingo_index.rand(0..4)
+    @number = rand(1..100)
 
   end
 
-  def generate
-    @bingo = {"B" => 0,"I" => 1,"N" => 2,"G" => 3,"0" => 4}
-    @num = rand(1..100)
+  def random_select
+    case
+     when @bingo_index_random == 0 then puts "B #{@number}"
+     when @bingo_index_random == 1 then puts "I #{@number}"
+     when @bingo_index_random == 2 then puts "N #{@number}"
+     when @bingo_index_random == 3 then puts "G #{@number}"
+     when @bingo_index_random == 4 then puts "O #{@number}"
+    end
   end
 
   def call
-    @bingo_board
-
+    if @bingo_board[@bingo_index_random].include?(@number)
+    @bingo_board[@bingo_index_random].map! {|row| row == @number? "X" : row}
+  else
+    puts @bingo_board
+    end
   end
-
-  def check
-
-  end
-
 end
-
-#tic_tac_toe.each do |row|
-#  row.each do |cell|
-#    if cell == ""
-#      cell = "X"
 
 
 
@@ -75,11 +78,27 @@ new_game = BingoBoard.new(board)
 =begin
 
 How difficult was pseudocoding this challenge? What do you think of your pseudocoding style?
-What are the benefits of using a class for this challenge?
-How can you access coordinates in a nested array?
-What methods did you use to access and modify the array?
-Give an example of a new method you learned while reviewing the Ruby docs. Based on what you see in the docs, what purpose does it serve, and how is it called?
-How did you determine what should be an instance variable versus a local variable?
-What do you feel is most improved in your refactored solution?
+
+# I found the outline a bit hard to follow this time around to PC. The notes of create vs define confused me a bit, but think it came together eventually.
+
+# What are the benefits of using a class for this challenge?
+
+# There many variables involved in this program and using class is an efficient way of creating methods and outlining the behavior of this game.
+
+# How can you access coordinates in a nested array?
+
+# Indices are the way to access coordinates in nested arrays.
+
+# What methods did you use to access and modify the array?
+
+# The include? and replace methods are good to utilize to accessa and modify array.
+
+# Give an example of a new method you learned while reviewing the Ruby docs. Based on what you see in the docs, what purpose does it serve, and how is it called?
+
+# I've learned about the map method and it's benefits to iterate  throughout the array to create a new array.
+
+# How did you determine what should be an instance variable versus a local variable?
+
+# Instance variables are those that should be used throughout the program.
 
 =end
