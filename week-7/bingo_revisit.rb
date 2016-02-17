793f9c132b715fd12ce2194043c28bb84f47d251
Saralis Rivera <saralis.rivera@gmail.com>
2016-02-17 15:13:09 -0500

    week 7 update to game and additional assignment

---
+++ week-7/bingo_revisit.rb
@@ -0,0 +1,62 @@
+# A Nested Array to Model a Bingo Board SOLO CHALLENGE
+
+# I spent [#] hours on this challenge.
+
+
+# Release 0: Pseudocode
+# Outline:
+
+# Create a method to generate a letter ( b, i, n, g, o) and a number (1-100)
+
+# Check the called column for the number called.
+  #fill in the outline here
+
+# If the number is in the column, replace with an 'x'
+  #fill in the outline here
+
+# Display a column to the console
+  #fill in the outline here
+
+# Display the board to the console (prettily)
+  #fill in the outline here
+
+# Initial Solution
+
+class BingoBoard
+
+  def initialize(board)
+    @bingo_board = board
+  end
+
+  def call
+    @letter = ["B", "I", "N", "G", "O"]
+    @num = rand(1..100)
+    @bingo_call = @letter.sample + @num
+  end
+
+puts @bingo_call
+
+  def check
+    if @bingo_call == @bingo_board
+    end
+  end
+
+end
+
+
+#puts "Check #{@letter} #{@num}"
+# Refactored Solution
+
+
+
+#DRIVER CODE (I.E. METHOD CALLS) GO BELOW THIS LINE
+board = [[47, 44, 71, 8, 88],
+        [22, 69, 75, 65, 73],
+        [83, 85, 97, 89, 57],
+        [25, 31, 96, 68, 51],
+        [75, 70, 54, 80, 83]]
+
+#new_game = BingoBoard.new(board)
+
+
+#Reflection
